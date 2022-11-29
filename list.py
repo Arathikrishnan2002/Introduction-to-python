55377ec362a1478181409d86e909fe8bc8774611
R Arathi krishnan <85704406+Arathikrishnan2002@users.noreply.github.com>
2022-11-29 23:45:27 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,25 @@
+values = ["Java","Python","C"]
+
+#printing list
+
+print(values)
+
+#replacing
+
+value[1] = "Ruby"
+print(values)
+
+#aadding values
+
+values = values + ["JS","HTML",10]
+print(values)
+
+#inserting values into last
+
+values.apppend("PHP")
+print(values)
+
+#reading 
+
+values.append(input("Enter a language"))
+print(values)
