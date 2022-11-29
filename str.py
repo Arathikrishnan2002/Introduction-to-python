74352e279fff88d2e085788146c2f4df459687b3
R Arathi krishnan <85704406+Arathikrishnan2002@users.noreply.github.com>
2022-11-29 23:17:14 +0530

    Create str.py

---
+++ str.py
@@ -0,0 +1,17 @@
+a = "introduction to python"
+
+#printing length 
+
+print(len(a))
+
+#printing particular character
+
+print(a[5])
+
+#printing within limit
+
+print(a[4:8])
+
+#printing in uppercase
+
+print(a.uper())
