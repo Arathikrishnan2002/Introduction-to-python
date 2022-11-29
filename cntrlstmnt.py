f259addf9f4ad4bfde33ebc7f3ce91cb76118f49
R Arathi krishnan <85704406+Arathikrishnan2002@users.noreply.github.com>
2022-11-30 00:19:06 +0530

    Update cntrlstmnt.py

--- cntrlstmnt.py
+++ cntrlstmnt.py
@@ -8,3 +8,16 @@ elif num == 0:
   print("number is zero")
 else :
   print("number is positive")
+
+  #while
+  i =1
+  
+  while(i<=10) :
+    printf(i)
+    i=i+1
+    
+ #for
+
+files = ["a","b","c"]
+for x in files :
+  print(x)
