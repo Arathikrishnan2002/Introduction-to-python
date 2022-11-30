ffba67646784f80fd19e18a7bce178e2ea7d7901
R Arathi krishnan <85704406+Arathikrishnan2002@users.noreply.github.com>
2022-11-30 23:27:25 +0530

    Create funx.py

---
+++ funx.py
@@ -0,0 +1,16 @@
+def hey():
+  print("HEllO WORLD")
+ 
+hey()
+
+#argument passing
+
+def one(name,age):
+  print("My name is"+name+"age"+str(age))
+ 
+def two():
+  print("hello")
+ 
+value = "Ajith"
+two()
+one(value,1)
