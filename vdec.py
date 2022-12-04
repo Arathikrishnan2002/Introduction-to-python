39fd5fe33fb8631c3030873b748f960640a97419
R Arathi krishnan <85704406+Arathikrishnan2002@users.noreply.github.com>
2022-12-04 21:11:12 +0530

    Create vdec.py

---
+++ vdec.py
@@ -0,0 +1,41 @@
+
+'''
+value = 10
+
+def sample():
+    print(value)
+    
+    
+print (value )
+
+'''
+'''
+value = 10
+
+def sample():
+    print(value)
+    
+    
+print (value )
+sample()
+
+'''
+'''
+value = 10
+
+def sample():
+    value =5
+    print(value)
+    
+sample()    
+print (value )
+'''
+value = 10
+
+def sample():
+  
+    s=value+1
+    print(s)
+    
+sample()    
+print (value )
